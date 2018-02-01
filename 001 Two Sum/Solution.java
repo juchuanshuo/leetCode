79709e133f72e4d83d50b119e6a1235106acc92f
据传说 <1304363358@qq.com>
2018-02-01 19:43:17 +0800

    Update Solution.java

--- 001 Two Sum/Solution.java
+++ 001 Two Sum/Solution.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        //停止循环的标志
-        boolean end = false;
         for(int i = 0; i<nums.length; i++){
             for(int j = 0; j<nums.length; j++){
                 if( i == j){
@@ -12,12 +10,8 @@ class Solution {
                 if(target == (nums[i] + nums[j])){
                     result[0] = i;
                     result[1] = j;
-                    end = true;
-                    break;
+                    return result;
                 }
-            }
-            if(end){//当得到结果时，终止接下来的操作
-                break;
             }
         }
         return result;
