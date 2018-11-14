38f1be76b3e84274a60014e7d1253f58ac8cb49d
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2018-11-14 14:55:20 +0800

    重新更改下第一题的格式

---
+++ src/cn/com/jcs/leetCode/problem1/twoSum/Solution.java
@@ -0,0 +1,24 @@
+package cn.com.jcs.leetCode.problem1.twoSum;
+
+/**
+ * 值得纪念的第一题
+ */
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (i == j) {
+					// 当数组下标一致时跳过此循环，因为每个数字仅被使用1次
+					continue;
+				}
+				if (target == (nums[i] + nums[j])) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ src/cn/com/jcs/leetCode/problem1/twoSum/Test.java
@@ -0,0 +1,10 @@
+package cn.com.jcs.leetCode.problem1.twoSum;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
