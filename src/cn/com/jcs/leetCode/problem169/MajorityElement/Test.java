0cf4b94215c3c9f79ff352d15312bcc20871e6f8
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2018-11-27 16:26:01 +0800

    题目169

---
+++ src/cn/com/jcs/leetCode/problem169/MajorityElement/Test.java
@@ -0,0 +1,10 @@
+package cn.com.jcs.leetCode.problem169.MajorityElement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Solution().majorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
+	}
+
+}
