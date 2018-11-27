9b0fabfe72b8ab7bfb0981d50b10db078db5a7bf
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2018-11-27 16:30:54 +0800

    问题217

---
+++ src/cn/com/jcs/leetCode/problem217/ContainsDuplicate/Test.java
@@ -0,0 +1,16 @@
+package cn.com.jcs.leetCode.problem217.ContainsDuplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<Integer> set = new HashSet<Integer>();
+    	set.add(100);
+    	set.add(100);
+    	System.out.println(set.size());
+	}
+
+}
