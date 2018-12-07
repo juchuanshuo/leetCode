f1f4d6c5c73e2fdca3d37d71c7da08cc5714bda0
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2018-12-07 14:10:21 +0800

    题目27

---
+++ src/cn/com/jcs/leetCode/problem27/RemoveElement/Test.java
@@ -0,0 +1,10 @@
+package cn.com.jcs.leetCode.problem27.RemoveElement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Solution().removeElement(new int[] { 1,1,34,2,3,4 }, 1));
+	}
+
+}
