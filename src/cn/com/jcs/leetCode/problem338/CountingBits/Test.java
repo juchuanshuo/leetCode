d38d14022bf237dbb1615b55166e0e5040bbf12b
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2018-12-24 17:28:55 +0800

    题目338

---
+++ src/cn/com/jcs/leetCode/problem338/CountingBits/Test.java
@@ -0,0 +1,13 @@
+package cn.com.jcs.leetCode.problem338.CountingBits;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new Solution().countBits(5);
+		for (int b : a) {
+			System.out.println(b);
+		}
+	}
+
+}
