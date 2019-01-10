8b77e908a1828c99b2b9d6257964425b4b5c17c1
zhaoxiaoming <zhaoxiaoming@agree.com.cn>
2019-01-10 14:03:23 +0800

    题目389和412

---
+++ src/cn/com/jcs/leetCode/problem412/fizzBuzz/Test.java
@@ -0,0 +1,10 @@
+package cn.com.jcs.leetCode.problem412.fizzBuzz;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Solution().fizzBuzz(12));
+	}
+
+}
